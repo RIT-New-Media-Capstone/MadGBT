b61264a2fb8ae8f5eeb9c0dda4401822b8e96348
Felix Warner (RIT Student) <fgw7468@rit.edu>
2023-11-16 14:31:04 -0500

    Refactored project basis to have client and server in respect to OpenAI private keys

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const url = require('url');
+const htmlHandler = require('./htmlResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/bundle.js': htmlHandler.getJS,
+  '/logo.svg': htmlHandler.getLogo,
+  notFound: htmlHandler.getIndex,
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url, true);
+  const handlerFunction = urlStruct[parsedUrl.pathname];
+  const { query } = parsedUrl;
+  if (handlerFunction) {
+    handlerFunction(request, response, query);
+  } else {
+    urlStruct.notFound(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
